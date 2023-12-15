13178032bcf3221052cec49a1b65b1a3c97aaf25
seansaniel2131 <106427039+seansaniel2131@users.noreply.github.com>
2023-12-16 00:50:32 +0800

    Update save_appointment.php

--- save_appointment.php
+++ save_appointment.php
@@ -1,17 +1,14 @@
 <?php
-// Database connection
 $servername = "localhost";
 $username = "jerrmonddentalcl_sean";
 $password = "5ipFwFDgVR^8";
 $dbname = "jerrmonddentalcl_proj1";
 
-// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Capture form data
 $firstName = $_POST['firstName'];
 $contactNumber = $_POST['contactNumber'];
 $hmoBenefactor = $_POST['hmoBenefactor'];
@@ -19,15 +16,12 @@ $hmoPartner = $_POST['hmoPartner'];
 $appointmentDate = date("Y-m-d", strtotime($_POST['appointmentDate']));
 $appointmentTime = date("H:i:s", strtotime($_POST['appointmentTime']));
 
-// Prepare and execute the SQL statement
 $sql = "INSERT INTO Appointments (FirstName, ContactNumber, HMO_Benefactor, HMO_Partner, AppointmentDate, AppointmentTime) 
         VALUES ('$firstName', '$contactNumber', '$hmoBenefactor', '$hmoPartner', '$appointmentDate', '$appointmentTime')";
         
 if ($conn->query($sql) === TRUE) {
-    // Close the database connection
     $conn->close();
     
-    // Redirect back to the schedule page
     header("Location: schedule.php");
     exit();
 } else {
